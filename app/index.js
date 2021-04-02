75c645266387eb2a79bf4cb63f61a78a3a1a57c2
Chris Gorman <chrisjohgorman@gmail.com>
2021-04-02 10:11:18 -0400

    Added mercury to planet library added testcase for mercury to index.js

--- app/index.js
+++ app/index.js
@@ -1,5 +1,16 @@
 import * as pc from "../common/planetCoordinates.js";
 
-const uranus = pc.uranus(pc.dayNumber(2021,4,1), 45.3661, -75.7900);
+const lat = 45.3661;
+const long = -75.7900;
+const d = new Date();
+
+const ut = d.getUTCHours() + d.getUTCMinutes()/60 +
+    d.getUTCSeconds()/3600;
+const mercury = pc.mercury(pc.dayNumber(d.getFullYear(),
+    (d.getMonth() + 1),d.getDate()), lat, long, ut);
+console.log("mercury azimuth %s", mercury.az.toFixed(2));
+console.log("mercury altitude %s", mercury.alt.toFixed(2));
+const uranus = pc.uranus(pc.dayNumber(d.getFullYear(),
+    (d.getMonth() + 1),d.getDate()), lat, long);
 console.log("uranus azimuth %s", uranus.az.toFixed(2));
 console.log("uranus altitude %s", uranus.alt.toFixed(2));
