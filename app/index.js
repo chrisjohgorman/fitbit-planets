9419e8a98a9098f011702f660fe7424cb8c63775
Chris Gorman <chrisjohgorman@gmail.com>
2021-04-05 15:32:28 -0400

    Fixed string display
    - using %s appears to be improper.  One should use + variable.

--- app/index.js
+++ app/index.js
@@ -1,48 +1,53 @@
+import { geolocation } from "geolocation";
+import document from "document";
 import * as pc from "../common/planetCoordinates.js";
 
-const lat = 45.3661;
-const long = -75.7900;
-const d = new Date();
+  //FIXME - remove
+  //const lat = 45.3661;
+  //const long = -75.7900;
+  const d = new Date();
+  console.log("latitude:" + lat); 
+  console.log("longitude:" + long); 
+  const ut = d.getUTCHours() + d.getUTCMinutes()/60 +
+      d.getUTCSeconds()/3600;
+  const sun = pc.sun(pc.dayNumber(d.getFullYear(),
+      (d.getMonth() + 1),d.getDate()), lat, long);
+  console.log("sun azimuth: " + sun.az.toFixed(2));
+  console.log("sun altitude: " + sun.alt.toFixed(2));
+  const mercury = pc.mercury(pc.dayNumber(d.getFullYear(),
+      (d.getMonth() + 1),d.getDate()), lat, long, ut);
+  console.log("mercury azimuth: " + mercury.az.toFixed(2));
+  console.log("mercury altitude: " + mercury.alt.toFixed(2));
+  const venus = pc.venus(pc.dayNumber(d.getFullYear(),
+      (d.getMonth() + 1),d.getDate()), lat, long, ut);
+  console.log("venus azimuth: " + venus.az.toFixed(2));
+  console.log("venus altitude: " + venus.alt.toFixed(2));
+  const moon = pc.moon(pc.dayNumber(d.getFullYear(),
+      (d.getMonth() + 1),d.getDate()), lat, long, ut);
+  console.log("moon azimuth: " + moon.az.toFixed(2));
+  console.log("moon altitude: " + moon.alt.toFixed(2));
+  const mars = pc.mars(pc.dayNumber(d.getFullYear(),
+      (d.getMonth() + 1),d.getDate()), lat, long, ut);
+  console.log("mars azimuth: " + mars.az.toFixed(2));
+  console.log("mars altitude: " + mars.alt.toFixed(2));
+  const jupiter = pc.jupiter(pc.dayNumber(d.getFullYear(),
+      (d.getMonth() + 1),d.getDate()), lat, long, ut);
+  console.log("jupiter azimuth: " + jupiter.az.toFixed(2));
+  console.log("jupiter altitude: " + jupiter.alt.toFixed(2));
+  const saturn = pc.saturn(pc.dayNumber(d.getFullYear(),
+      (d.getMonth() + 1),d.getDate()), lat, long);
+  console.log("saturn azimuth: " + saturn.az.toFixed(2));
+  console.log("saturn altitude: " + saturn.alt.toFixed(2));
+  const uranus = pc.uranus(pc.dayNumber(d.getFullYear(),
+      (d.getMonth() + 1),d.getDate()), lat, long);
+  console.log("uranus azimuth: " + uranus.az.toFixed(2));
+  console.log("uranus altitude: " + uranus.alt.toFixed(2));
+  const neptune = pc.neptune(pc.dayNumber(d.getFullYear(),
+      (d.getMonth() + 1),d.getDate()), lat, long);
+  console.log("neptune azimuth: " + neptune.az.toFixed(2));
+  console.log("neptune altitude: " + neptune.alt.toFixed(2));
+  const pluto = pc.pluto(pc.dayNumber(d.getFullYear(),
+      (d.getMonth() + 1),d.getDate()), lat, long);
+  console.log("pluto azimuth: " + pluto.az.toFixed(2));
+  console.log("pluto altitude: " + pluto.alt.toFixed(2));
 
-const ut = d.getUTCHours() + d.getUTCMinutes()/60 +
-    d.getUTCSeconds()/3600;
-const sun = pc.sun(pc.dayNumber(d.getFullYear(),
-    (d.getMonth() + 1),d.getDate()), lat, long);
-console.log("sun azimuth %s", sun.az.toFixed(2));
-console.log("sun altitude %s", sun.alt.toFixed(2));
-const mercury = pc.mercury(pc.dayNumber(d.getFullYear(),
-    (d.getMonth() + 1),d.getDate()), lat, long, ut);
-console.log("mercury azimuth %s", mercury.az.toFixed(2));
-console.log("mercury altitude %s", mercury.alt.toFixed(2));
-const venus = pc.venus(pc.dayNumber(d.getFullYear(),
-    (d.getMonth() + 1),d.getDate()), lat, long, ut);
-console.log("venus azimuth %s", venus.az.toFixed(2));
-console.log("venus altitude %s", venus.alt.toFixed(2));
-const moon = pc.moon(pc.dayNumber(d.getFullYear(),
-    (d.getMonth() + 1),d.getDate()), lat, long, ut);
-console.log("moon azimuth %s", moon.az.toFixed(2));
-console.log("moon altitude %s", moon.alt.toFixed(2));
-const mars = pc.mars(pc.dayNumber(d.getFullYear(),
-    (d.getMonth() + 1),d.getDate()), lat, long, ut);
-console.log("mars azimuth %s", mars.az.toFixed(2));
-console.log("mars altitude %s", mars.alt.toFixed(2));
-const jupiter = pc.jupiter(pc.dayNumber(d.getFullYear(),
-    (d.getMonth() + 1),d.getDate()), lat, long, ut);
-console.log("jupiter azimuth %s", jupiter.az.toFixed(2));
-console.log("jupiter altitude %s", jupiter.alt.toFixed(2));
-const saturn = pc.saturn(pc.dayNumber(d.getFullYear(),
-    (d.getMonth() + 1),d.getDate()), lat, long);
-console.log("saturn azimuth %s", saturn.az.toFixed(2));
-console.log("saturn altitude %s", saturn.alt.toFixed(2));
-const uranus = pc.uranus(pc.dayNumber(d.getFullYear(),
-    (d.getMonth() + 1),d.getDate()), lat, long);
-console.log("uranus azimuth %s", uranus.az.toFixed(2));
-console.log("uranus altitude %s", uranus.alt.toFixed(2));
-const neptune = pc.neptune(pc.dayNumber(d.getFullYear(),
-    (d.getMonth() + 1),d.getDate()), lat, long);
-console.log("neptune azimuth %s", neptune.az.toFixed(2));
-console.log("neptune altitude %s", neptune.alt.toFixed(2));
-const pluto = pc.pluto(pc.dayNumber(d.getFullYear(),
-    (d.getMonth() + 1),d.getDate()), lat, long);
-console.log("pluto azimuth %s", pluto.az.toFixed(2));
-console.log("pluto altitude %s", pluto.alt.toFixed(2));
